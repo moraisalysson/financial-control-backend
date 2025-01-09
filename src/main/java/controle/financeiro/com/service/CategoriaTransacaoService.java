b2f960f6dd7bcd6314f99692f159c3a98cd622ba
alysson.morais <alysson.morais@dataprev.gov.br>
2025-01-08 21:36:46 -0300

    implementacao dos logs

--- src/main/java/controle/financeiro/com/service/CategoriaTransacaoService.java
+++ src/main/java/controle/financeiro/com/service/CategoriaTransacaoService.java
@@ -10,6 +10,8 @@ import controle.financeiro.com.to.CategoriaTransacaoTO;
 import controle.financeiro.com.to.ContaBancariaTO;
 import controle.financeiro.com.to.form.CategoriaTransacaoForm;
 import controle.financeiro.com.to.form.ContaBancariaForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,7 @@ public class CategoriaTransacaoService {
 
     private final CategoriaTransacaoRepository repository;
     private final CategoriaTransacaoMapper mapper;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CategoriaTransacaoService.class);
 
     public CategoriaTransacaoService(CategoriaTransacaoRepository repository, CategoriaTransacaoMapper mapper) {
         this.repository = repository;
@@ -28,12 +31,15 @@ public class CategoriaTransacaoService {
     }
 
     public CategoriaTransacaoTO create(CategoriaTransacaoForm form) {
+        LOGGER.info("[CategoriaTransacao - Service] Iniciando a criação de uma Categoria de Transação");
         CategoriaTransacao entity = mapper.toEntityCreate(form);
 
         try {
             repository.save(entity);
+            LOGGER.info("[CategoriaTransacao - Service] Categoria de Transação criada com sucesso. ID: {}", entity.getId());
             return mapper.toTO(entity);
         } catch (DataAccessException e) {
+            LOGGER.error("[CategoriaTransacao - Service] Erro ao tentar criar a Categoria de Transação. Erro: {}", e.getMessage());
             throw new DataIntegrityViolationException("Não foi possível salvar a Categoria da Transação.");
         }
     }
